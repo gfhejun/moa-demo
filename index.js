1364467c512622774df4cead57462723ec9cb9df
onlyforhebe <342552254@qq.com>
2016-02-21 22:26:15 +0800

    add index

---
+++ index.js
@@ -0,0 +1,56 @@
+var Header = React.createClass({
+    render: function () {
+        return <div className="header">
+            {this.props.title}
+            </div>
+
+    }
+});
+
+var Footer = React.createClass({
+    render: function () {
+        return <div className="footer">
+            {this.props.title}
+        </div>
+
+    }
+});
+
+var Icon = React.createClass({
+    render: function () {
+        return <span>
+            <div id={this.props.id} className="icon"></div>
+        </span>
+    }
+});
+
+var icons = [
+    {name:"OA"},
+    {name:"ERP"}
+];
+
+var Content = React.createClass({
+    render: function () {
+        return <div className="content">
+            {
+                icons.map(function(icon){
+                    return <Icon key={icon.name} id={icon.name}/>
+                })
+            }
+        </div>
+
+    }
+});
+
+ReactDOM.render(
+    <Header title="Home"/>,
+    document.getElementById("header")
+);
+ReactDOM.render(
+    <Footer title="Footer"/>,
+    document.getElementById("footer")
+);
+ReactDOM.render(
+    <Content title="Content"/>,
+    document.getElementById("content")
+);
